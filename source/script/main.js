c9d055297dd40c68f8f518d591bb0a5523d194f6
Rob Moran <rob.moran@arm.com>
2016-03-26 20:51:15 +0000

    Removed un-needed model fetch

--- source/script/main.js
+++ source/script/main.js
@@ -8,10 +8,8 @@ import React from 'react';
 import {render} from 'react-dom';
 
 import MenuComponent from '../components/menu.jsx';
-import NeoCollection from '../collections/neo';
 
 render(<MenuComponent/>, document.getElementById('menu'));
-NeoCollection.fetch();
 
 $(function() {
 	$(document).foundation();
